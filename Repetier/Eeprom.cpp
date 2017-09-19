5f91e9f55832f1a2282c10ea0b0ad2ff944874e8
Stefan Bek <Nibbels@users.noreply.github.com>
2017-09-19 23:20:21 +0200

    remove garbage

--- Repetier/Eeprom.cpp
+++ Repetier/Eeprom.cpp
@@ -64,7 +64,6 @@ void EEPROM::update(GCode *com)
 void EEPROM::restoreEEPROMSettingsFromConfiguration()
 {
 #if EEPROM_MODE!=0
-    //uint8_t version = EEPROM_PROTOCOL_VERSION;
     baudrate = BAUDRATE;
     maxInactiveTime = MAX_INACTIVE_TIME*1000L;
     stepperInactiveTime = STEPPER_INACTIVE_TIME*1000L;
@@ -1226,7 +1225,6 @@ void EEPROM::writeSettings()
     for(uint8_t i=0; i<NUM_EXTRUDER; i++)
     {
         int o=EEPROM::getExtruderOffset(i);
-        //Extruder *e = &extruder[i];
         writeFloat(o+EPR_EXTRUDER_STEPS_PER_MM,Com::tEPRStepsPerMM);
         writeFloat(o+EPR_EXTRUDER_MAX_FEEDRATE,Com::tEPRMaxFeedrate);
         writeFloat(o+EPR_EXTRUDER_MAX_START_FEEDRATE,Com::tEPRStartFeedrate);
